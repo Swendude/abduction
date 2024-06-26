628401df03ef9f8ee448dae36a86915180f06c89
Swendude <swenmulderij@gmail.com>
2024-06-26 15:07:42 +0200

    a

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,157 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { useRef, useState } from "react";
+import {
+  CameraControls,
+  Grid,
+  OrthographicCamera,
+  Outlines,
+  RoundedBox,
+  Sphere,
+  Stage,
+  Box,
+  TransformControls,
+  PivotControls,
+  Environment,
+  Stars,
+  Point,
+  PointMaterial,
+  Wireframe,
+  Plane,
+  Sky,
+  GradientTexture,
+  MeshWobbleMaterial,
+  Center,
+  Resize,
+} from "@react-three/drei";
+import { Group, Mesh } from "three";
 
-const inter = Inter({ subsets: ["latin"] });
+import RocksLarge from "../models/RocksLarge";
+import Dish from "../models/Dish";
+
+const Player = ({ matColor }: { matColor: string }) => {
+  useFrame((state, delta) => {
+    boxRef.current.rotation.y += delta / 2;
+  });
+  const boxRef = useRef<Mesh>(null!);
+  return (
+    <group>
+      <Box ref={boxRef} position={[0, 1, 0]} castShadow>
+        <meshToonMaterial color={matColor} />
+        <Outlines thickness={0.04} color={"#000000"} />
+      </Box>
+    </group>
+  );
+};
+
+const Orbital = ({
+  initialRotation = 0,
+  color = "white",
+  size = 1,
+  distance = 10,
+}: {
+  initialRotation?: number;
+  color?: string;
+  size?: number;
+  distance?: number;
+}) => {
+  useFrame((state, delta) => {
+    if (!orbitalRef.current) return;
+    orbitalRef.current.rotation.x += delta / 4;
+  });
+
+  const orbitalRef = useRef<Group>(null);
+  return (
+    <object3D rotation-y={Math.PI * 0.25}>
+      <group ref={orbitalRef} rotation-x={initialRotation}>
+        {/* Control point (center) */}
+        <object3D position={[0, 0, 0]} />
+        <Sphere
+          args={[size]}
+          material-color={color}
+          position={[0, distance, 0]}
+        >
+          <Outlines thickness={0.04} color={"#000000"} />
+          <pointLight
+            intensity={Math.pow(1 + size, 7)}
+            castShadow
+            color={color}
+          />
+        </Sphere>
+      </group>
+    </object3D>
+  );
+};
 
 export default function Home() {
+  const [cubeCol, setCubeCol] = useState<string>("#ffffff");
+  const [enableGrid, setEnableGrid] = useState(false);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className={`flex flex-col items-center`}>
+      <h1 className="text-4xl font-bold">Hello React Three Fiber!</h1>
+
+      <div className="w-[800px] h-[800px] border border-black">
+        <Canvas shadows>
+          <OrthographicCamera
+            makeDefault
+            position={[-10, 10, 10]}
+            rotation-order={"YXZ"}
+            rotation-y={-Math.PI / 4}
+            rotation-x={Math.atan(-1 / Math.sqrt(2))}
+            left={-100}
+            right={100}
+            top={100}
+            bottom={-100}
+            zoom={13}
+            near={0}
+            far={100}
+          />
+          <CameraControls makeDefault />
+
+          {enableGrid && <Grid position={[0, 0, 0]} args={[10, 10]} />}
+
+          <ambientLight color="#ffffff" intensity={0.7} />
+
+          <Orbital color="#fbe980" distance={8} size={0.8} />
+          <Orbital
+            color="#f4f1e6"
+            size={0.3}
+            distance={8}
+            initialRotation={Math.PI}
+          />
+
+          <Player matColor={cubeCol} />
+          <group>
+            <Box
+              position={[0, -1.65, 0]}
+              args={[30, 30, 1]}
+              rotation-x={Math.PI * -0.5}
+              rotation-z={Math.PI * -0.25}
+            >
+              <meshStandardMaterial color={"#4597cf"}></meshStandardMaterial>
+            </Box>
+            <Box args={[10, 1, 10]} position={[0, -0.55, 0]} receiveShadow>
+              <meshToonMaterial color={"rgb(106, 188, 106)"} />
+              <Outlines thickness={0.04} color={"#000"} />
+            </Box>
+          </group>
+
+          <RocksLarge position={[4, 0, 3]} scale={3} castShadow />
+          <Dish position={[-3, 0, -3]} rotation-y={Math.PI * -1.5} scale={2} />
+        </Canvas>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <input
+        type="color"
+        name="color"
+        value={cubeCol}
+        id="color"
+        onChange={(e) => setCubeCol(e.currentTarget.value)}
+      />
+      <input
+        type="button"
+        value="toggle"
+        onClick={(e) => setEnableGrid(!enableGrid)}
+      />
     </main>
   );
 }
